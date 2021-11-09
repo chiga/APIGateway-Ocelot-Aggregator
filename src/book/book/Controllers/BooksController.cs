066d8567f7f8ca6ede900ef79a7bb801ae3c6977
chiga <marcelochiga@frwk.com.br>
2021-11-09 09:29:55 -0300

    create book API

---
+++ src/book/book/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using book.Helpers;
+using book.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace book.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IEnumerable<Book> Books;
+
+        public BooksController()
+        {
+            Books = BookFactoryHelper.CreateBooks();
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Books;
+        }
+
+        [HttpGet("{code:guid}")]
+        public Book Get(Guid code)
+        {
+            return Books.Where(b => b.Code == code).SingleOrDefault();
+        }
+    }
+}
